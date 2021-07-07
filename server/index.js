394503209abcb53ad3d693cd1138e4ed5ea0b58c
Daniel <dartiles1997@gmail.com>
2021-07-07 19:46:50 +0000

    modificado indexjs

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server que paso!" });
+    res.json({ message: "Hello from server! Modiuficado desde rama Daniel y subir a development" });
 });
 
 app.listen(PORT, () => {
