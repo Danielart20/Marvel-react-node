c127e5fc70093442e0dfb99448c0cb64dc000405
Daniel <dartiles1997@gmail.com>
2021-07-16 16:18:00 +0000

    servidor con apis de coimics..

--- server/index.js
+++ server/index.js
@@ -6,8 +6,35 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    axios.get('http://gateway.marvel.com/v1/public/characters?ts=1&apikey='+ API_KEY +'&hash=1e1fbb64cbc9f5ab7a0b644b433c716c')
+app.get("/api/characters", (req, res) => {
+    axios.get('http://gateway.marvel.com/v1/public/characters?orderBy=modified&limit=4&ts=1&apikey='+ API_KEY +'&hash=1e1fbb64cbc9f5ab7a0b644b433c716c')
+    .then(response => {
+        res.json({ message: response.data});
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
+app.get("/api/comics", (req, res) => {
+    axios.get('http://gateway.marvel.com/v1/public/comics?orderBy=onsaleDate&limit=4&ts=1&apikey='+ API_KEY +'&hash=1e1fbb64cbc9f5ab7a0b644b433c716c')
+    .then(response => {
+        res.json({ message: response.data});
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
+app.get("/api/series", (req, res) => {
+    axios.get('http://gateway.marvel.com/v1/public/series?orderBy=startYear&limit=4&ts=1&apikey='+ API_KEY +'&hash=1e1fbb64cbc9f5ab7a0b644b433c716c')
+    .then(response => {
+        res.json({ message: response.data});
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
+app.get("/api/events", (req, res) => {
+    axios.get('http://gateway.marvel.com/v1/public/events?orderBy=startDate&limit=4&ts=1&apikey='+ API_KEY +'&hash=1e1fbb64cbc9f5ab7a0b644b433c716c')
     .then(response => {
         res.json({ message: response.data});
     })
